70ff0cb1ff498f02adac8b9ee306bb2e9f064188
HenryTacuri <henrytacuri2@gmail.com>
2024-07-04 10:16:56 -0500

    Commit 5

--- src/main/java/ec/edu/ups/ppw63/carros_crud/business/GestionCarros.java
+++ src/main/java/ec/edu/ups/ppw63/carros_crud/business/GestionCarros.java
@@ -1,9 +1,5 @@
 package ec.edu.ups.ppw63.carros_crud.business;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 
 import ec.edu.ups.ppw63.carros_crud.dao.CarroDAO;
@@ -25,7 +21,6 @@ public class GestionCarros {
 		}else {
 			daoCarro.insert(carro);
 		}
-		this.enviarCorreo("Registro de carro", "Se registra un carro con codigo " + carro.getCodigo());
 	}
 	
 	
@@ -36,7 +31,6 @@ public class GestionCarros {
 		}else {
 			throw new Exception("Carro no existe");
 		}
-		this.enviarCorreo("Actualización de carro", "Se actualiza un carro con codigo " + carro.getCodigo());
 	}
 	
 
@@ -52,40 +46,11 @@ public class GestionCarros {
 	public void borrarCarro(int codigo){
 		
 		daoCarro.remove(codigo);
-		this.enviarCorreo("Eliminación de carro", "Se elimina un carro con codigo " + codigo);
 	}
 	
 
 	public List<Carro> getCarros(){
 		return daoCarro.getAll();
-	}
-	
-	
-	private void enviarCorreo(String subject, String body) {
-        String apiEndpoint = "http://10.128.0.9:8080/correos/rs/sendEmail";
-
-        String mensaje = "{\"subject\": \"" + subject + "\", \"body\": \"" + body + "\"}";
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-        
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiEndpoint))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(mensaje))
-                .build();
-
-        
-        try {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            int statusCode = response.statusCode();
-            String responseBody = response.body();
-
-            System.out.println("Status code: " + statusCode);
-            System.out.println("Response body: " + responseBody);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
 	}
 	
 }
